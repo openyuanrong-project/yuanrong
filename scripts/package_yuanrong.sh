17d8358d87281c872ea290ab07029de0afe91ad7
mayuehit <mayue51@huawei.com>
2025-12-01 15:36:11 +0800

    fix

--- scripts/package_yuanrong.sh
+++ scripts/package_yuanrong.sh
@@ -125,6 +125,9 @@ fi
 
 mv ${OUTPUT_DIR}/openyuanrong/functionsystem/deploy/third_party ${OUTPUT_DIR}/openyuanrong/
 mv ${OUTPUT_DIR}/openyuanrong/functionsystem/deploy/function_system/* ${OUTPUT_DIR}/openyuanrong/functionsystem/deploy/
+rm -rf ${OUTPUT_DIR}/openyuanrong/functionsystem/deploy/function_system/
+mv ${OUTPUT_DIR}/openyuanrong/functionsystem/deploy/vendor/etcd ${OUTPUT_DIR}/openyuanrong/functionsystem/deploy/third_party/
+rm -rf ${OUTPUT_DIR}/openyuanrong/functionsystem/deploy/vendor
 if [ -d ${OUTPUT_DIR}/openyuanrong/third_party/ ]; then
   find ${OUTPUT_DIR}/openyuanrong/third_party/ -type f -exec chmod 550 {} \;
 fi
